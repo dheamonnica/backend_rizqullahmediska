84aa3ff5c1f0ab90849b0a53a3a1145984662081
dheamonnica <dheamonnica.acc@gmail.com>
2024-03-06 01:19:04 +0700

    2 commit

---
+++ application/config/site.php
@@ -0,0 +1,13 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Base Site URL
+|--------------------------------------------------------------------------
+|
+| Your site name,
+|
+*/
+$config['site_name'] = 'rizqullahmediska';
+$config['version'] = '3.4.1';
